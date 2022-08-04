4dda551afcf9b106620e11924c2f5cfe1c31a69b
Rohit_Shrivastava <Rohit2.Shrivastava@ril.com>
2022-08-04 12:09:20 +0530

    orgin

--- demo.js
+++ demo.js
@@ -2,8 +2,8 @@
 
 function myFunction() {
     const x= document.getElementById("Product");
-    console.log(x.value)
-    var input = x.value;
+    // console.log(x.value)
+    const input = x.value;
     document.getElementById(input).scrollIntoView({behavior: 'smooth'});
 }
 
@@ -17,7 +17,7 @@ function toggle() {
     // const z = document.getElementById("myLinks");
     const z = document.querySelector(".header-links");
     // var z = document.getElementById("myLinks");
-    console.log(x.style.display);
+    // console.log(x.style.display);
     if (x.style.display === 'block' || window.innerWidth>960){
     x.style.display = 'none';
     y.style.display = 'block';
@@ -32,8 +32,9 @@ function toggle() {
     }
     // document.getElementById("myLinks").style.display = 'block';
 
-
 }
+
+
 
 
 
@@ -56,3 +57,45 @@ function scrollFunction() {
 function topFunction() {
     document.getElementById("Discover").scrollIntoView({behavior: 'smooth'});
 }
+
+// function myFunction2() {
+//     document.getElementById("Discover").scrollIntoView({behavior: 'smooth'});
+// }
+// setTimeout(myFunction2, 3000);
+// myFunction2();
+// catchVideo()
+//         .then(response => {
+//           console.log('Successfully fetched');
+//         })
+//         .catch(error => {
+//           console.log('error!');
+//           console.error(error);
+//         });
+
+ async function myFunction2(){     
+        const response = await fetch('jio_logo_blue.png');
+        const blob = await response.blob();
+        document.getElementById('jio_logo').src = URL.createObjectURL(blob);
+        document.getElementById('jio_logo').width = 71;
+        document.getElementById('jio_logo').height = 70;
+        setTimeout(myFunction3, 2000);
+    }
+setInterval(myFunction2, 4000);
+    async function myFunction3(){     
+        const response = await fetch('jio_red.svg');
+        const blob = await response.blob();
+        document.getElementById('jio_logo').src = URL.createObjectURL(blob);
+    }
+
+
+function getImage(){
+        url = "https://dog.ceo/api/breeds/image/random";
+        fetch(url).then((response)=>{
+            return response.json();
+        }).then((data)=>{
+            console.log(data);
+            document.getElementById("LastImage").src = data.message;
+        })
+    }
+
+getImage();
